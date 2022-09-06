42cf6804a64dfdb5b08a5948cdf94abe3348236a
devin-lynch <devin.l.lynch@gmail.com>
2022-09-06 14:32:02 -0500

    Work on post route

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // required packages
 const express = require('express')
 const layout = require('express-ejs-layouts')
+const fs = require('fs')
 
+// get the dinos from the db
+const readDinoFile = () => {
+        // use the filesystem to read the dino json
+        const dinosaurs = fs.readFileSync('./dinosaurs.json')
+        // parse the file into json data
+        const dinoData = JSON.parse(dinosaurs)
+        return dinoData
+}
 
 // express app config
 const app = express()
@@ -16,7 +25,12 @@ app.get('/', (req, res) => {
 
 // GET /dinosaurs -- show all dinos
 app.get('/dinosaurs', (req, res) => {
-    res.send('Show all dinos')
+    const dinoData = readDinoFile()
+    // console.log(dinosaurs)
+    // console.log(dinoData)
+    // send the dino info to the client
+    // TODO: add ejs view
+    res.json(dinoData)
 })
 
 // GET /dinosaurs/new -- display a form to create a new dino
@@ -26,12 +40,24 @@ app.get('/dinosaurs/new', (req, res) => {
 
 // POST /dinosaurs -- create a new dino in the DB
 app.post('/dinosaurs', (req, res) => {
-    res.send('Create a new dino in the DB')
+    // read the dino file
+    const dinoData = readDinoFile()
+    // payload of data from the request body (req.body)
+    // push the data payload into the array of dinos
+    console.log(req.body)
+    // save the dino file
+    // on POST routes -- DO NOT RENDER A TEMPLATE (this rule can be broken)
+    // redirect to where you can find a template
 })
 
 // GET /dinosaurs/:id -- display the details of one specific dino
 app.get('/dinosaurs/:id', (req, res) => {
-    res.send(`Show details for dino with id of ${req.params.id}`)
+    // get the dinos from the file
+    const dinoData = readDinoFile()
+    // look up array index from the url route params
+    const dino = dinoData[req.params.id]
+    // send back a single dino
+    res.json(dino)
 })
 
 // listen on a port
