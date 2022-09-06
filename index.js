2941758f37c6a0517330c7c90d0936d22ad660e8
devin-lynch <devin.l.lynch@gmail.com>
2022-09-06 13:52:11 -0500

    Start routes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const layout = require('express-ejs-layouts')
 
 // express app config
 const app = express()
-const PORT = 3001
+const PORT = 3002
 app.set('view engine', 'ejs')
 app.use(layout)
 
@@ -14,7 +14,25 @@ app.get('/', (req, res) => {
     res.send(`Welcome to the dino CRUD app 🦖`)
 })
 
-// listen on a port
-app.listen(PORT, () => {
-    console.log(`is that dinos I hear on port ${PORT} 🦕`)
+// GET /dinosaurs -- show all dinos
+app.get('/dinsaurs', (req, res) => {
+    res.send('Show all dinos')
 })
+
+// GET /dinosaurs/new -- display a form to create a new dino
+app.get('/dinosaurs/new', (req, res) => {
+    res.send('Show a form to create a new dino')
+})
+
+// POST /dinosaurs -- create a new dino in the DB
+app.post('/dinosaurs', (req, res) => {
+    res.send('Create a new dino in the DB')
+})
+
+// GET /dinosaurs/:id -- display the details of one specific dino
+app.get('/dinosaurs/:id', (req, res) => {
+    res.send(`Show details for dino with id of ${req.params.id}`)
+})
+
+// listen on a port
+app.listen(PORT, () => console.log(`is that dinos I hear on port ${PORT} 🦕`))
